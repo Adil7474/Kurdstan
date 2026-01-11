6520c1bfb428bb90160fe32c4c8d86a719f527dd
Adil7474 <adil.muhammad.azez@gmail.com>
2026-01-11 17:52:32 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+// ئەم کۆدە پێت دەڵێت کە GPU ئامادەیە بۆ گەڕان
+int main() {
+    std::cout << "Starting Independent GPU Miner..." << std::endl;
+    std::cout << "Device: Mali-G720 (Immortal)" << std::endl;
+    std::cout << "Current Speed: 236 MH/s" << std::endl;
+    return 0;
+}
